276a21917c5770769fd9d580084ce8dab69b7d52
Hyeonmin Park <hyeonmin.park@kennysoft.kr>
2021-04-26 10:17:58 +0900

    Initial commit from Spring Initializr

---
+++ src/main/java/kr/kennysoft/dependabotgradleext/DependabotGradleExtApplication.java
@@ -0,0 +1,13 @@
+package kr.kennysoft.dependabotgradleext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependabotGradleExtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependabotGradleExtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/kennysoft/dependabotgradleext/DependabotGradleExtApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kennysoft.dependabotgradleext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependabotGradleExtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
